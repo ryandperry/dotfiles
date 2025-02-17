abc61b2ebaf63b01f8d1d9ded384a29b8ec82c6d
Ryan Perry <rperry21@vols.utk.edu>
2025-02-17 10:14:36 -0500

    upaded file path to template files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,14 +21,14 @@ vim.api.nvim_create_user_command('Pomo', function()
     local date = os.date('%F')
     vim.api.nvim_put({ date }, 'c', true, true)
     local pomo_template = 
-        vim.fn.readfile(vim.fn.expand('./write/pomo.txt'))
+        vim.fn.readfile(vim.fn.expand('~/.config/nvim/write/pomo.txt'))
     vim.api.nvim_put(pomo_template, 'l', true, true)
 end, {})
 
 -- Writes Journal Template from ~/.journal_template.txt
 vim.api.nvim_create_user_command('Journal', function()
     local journal_template = 
-        vim.fn.readfile(vim.fn.expand('./write/journal.txt'))
+        vim.fn.readfile(vim.fn.expand('~/.config/nvim/write/journal.txt'))
     vim.api.nvim_put(journal_template, 'l', true, true)
 end, {})
 
